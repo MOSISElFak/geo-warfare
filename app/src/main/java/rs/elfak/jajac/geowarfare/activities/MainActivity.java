05ed088e3b7d65474652d9155f39b324de90b51e
Milos Jajac <milosjajac13@gmail.com>
2017-07-09 22:15:10 +0200

    Add basic map view

--- app/src/main/java/rs/elfak/jajac/geowarfare/activities/MainActivity.java
+++ app/src/main/java/rs/elfak/jajac/geowarfare/activities/MainActivity.java
@@ -3,45 +3,46 @@ package rs.elfak.jajac.geowarfare.activities;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import rs.elfak.jajac.geowarfare.R;
+import rs.elfak.jajac.geowarfare.fragments.MapFragment;
 
-public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.navigation_world);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.navigation_profile);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.navigation_rankings);
-                    return true;
-            }
-            return false;
-        }
-
-    };
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setOnNavigationItemSelectedListener(this);
+        navigation.setSelectedItemId(R.id.navigation_world);
     }
 
+    /* Try to find the fragment by tag first, if none exist, create
+    * a new one using the cls argument. */
+    private void replaceFragment(Fragment newFragment) {
+        FragmentManager fManager = getSupportFragmentManager();
+        fManager.beginTransaction()
+                .replace(R.id.main_fragment_container, newFragment)
+                .commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.navigation_world:
+                replaceFragment(new MapFragment());
+                return true;
+            case R.id.navigation_profile:
+                return true;
+            case R.id.navigation_rankings:
+                return true;
+        }
+        return false;
+    }
 }
---
+++ app/src/main/java/rs/elfak/jajac/geowarfare/fragments/MapFragment.java
@@ -0,0 +1,80 @@
+package rs.elfak.jajac.geowarfare.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+import rs.elfak.jajac.geowarfare.R;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback, View.OnClickListener {
+
+    private GoogleMap mGoogleMap;
+    private MapView mMapView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View inflatedView = inflater.inflate(R.layout.fragment_map, container, false);
+
+        FloatingActionButton buildFab = (FloatingActionButton) inflatedView.findViewById(R.id.map_fragment_build_button);
+        buildFab.setOnClickListener(this);
+
+        mMapView = (MapView) inflatedView.findViewById(R.id.map_fragment_map_view);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.getMapAsync(this);
+
+        return inflatedView;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+
+        mGoogleMap = googleMap;
+        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mMapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
