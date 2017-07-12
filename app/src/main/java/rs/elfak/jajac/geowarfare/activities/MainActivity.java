a35dd422bd668f19f637c1e9b6cd6fcd0f47d170
Milos Jajac <milosjajac13@gmail.com>
2017-07-12 23:30:53 +0200

    Change bottom navigation for simple main activity

--- app/src/main/java/rs/elfak/jajac/geowarfare/activities/MainActivity.java
+++ app/src/main/java/rs/elfak/jajac/geowarfare/activities/MainActivity.java
@@ -2,60 +2,71 @@ package rs.elfak.jajac.geowarfare.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import rs.elfak.jajac.geowarfare.R;
 import rs.elfak.jajac.geowarfare.fragments.MapFragment;
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class Main2Activity extends AppCompatActivity {
 
-    private MapFragment mMapFragment;
+    Spinner mFilterSpinner;
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(this);
-        navigation.setSelectedItemId(R.id.navigation_world);
-    }
+        mFilterSpinner = (Spinner) findViewById(R.id.toolbar_filter_spinner);
 
-    /* Try to find the fragment by tag first, if none exist, create
-    * a new one using the cls argument. */
-    private void replaceFragment(Fragment newFragment) {
-        FragmentManager fManager = getSupportFragmentManager();
-        fManager.beginTransaction()
-                .replace(R.id.main_fragment_container, newFragment)
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.main_fragment_container, new MapFragment())
                 .commit();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        ArrayAdapter<String> spinAdapter = new ArrayAdapter<String>(
+                this,
+                R.layout.toolbar_spinner_selected_item,
+                getResources().getStringArray(R.array.filter_array)
+        );
+        spinAdapter.setDropDownViewResource(R.layout.toolbar_spinner_dropdown_item);
+        mFilterSpinner.setAdapter(spinAdapter);
     }
 
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.navigation_world:
-                mMapFragment = new MapFragment();
-                replaceFragment(mMapFragment);
-                return true;
-            case R.id.navigation_profile:
-                return true;
-            case R.id.navigation_rankings:
-                return true;
-        }
-        return false;
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar_menu, menu);
+        return true;
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // Since Activity catches result before Fragment does, we pass it along
-        if (requestCode == MapFragment.REQUEST_CHECK_SETTINGS) {
-            mMapFragment.onActivityResult(requestCode, resultCode, data);
-        }
-        super.onActivityResult(requestCode, resultCode, data);
+    protected void onStart() {
+        super.onStart();
+        setupUI(mAuth.getCurrentUser());
     }
+
+    private void setupUI(FirebaseUser user) {
+
+    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        // Since Activity catches result before Fragment does, we pass it along
+//        if (requestCode == MapFragment.REQUEST_CHECK_SETTINGS) {
+//            mMapFragment.onActivityResult(requestCode, resultCode, data);
+//        }
+//        super.onActivityResult(requestCode, resultCode, data);
+//    }
 }
